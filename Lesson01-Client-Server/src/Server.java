6010ff677881bbf077a083b0e70bcd491d7d7801
rrenat358 <rrenat358@gmail.com>
2022-09-20 13:38:22 +0300

    1338

--- Lesson01-Client-Server/src/Server.java
+++ Lesson01-Client-Server/src/Server.java
@@ -1,11 +1,64 @@
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server implements Runnable {
+
+
+    private final int port;
+    private ServerSocket serverSocket;
+    private boolean isStopped;
+
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    private void openServerSocket() {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open server socket!", e);
+        }
+    }
 
 
 
 
-public class Server {
 
-    public static void main(String[] args) {
+
+
+
+    /**
+     * When an object implementing interface {@code Runnable} is used
+     * to create a thread, starting the thread causes the object's
+     * {@code run} method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method {@code run} is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
 
     }
+
+
+
+
+
+
+
+    public void stop() {
+        isStopped = true;
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
 
 }
--- Lesson01-Client-Server/src/ServerApp.java
+++ Lesson01-Client-Server/src/ServerApp.java
@@ -4,7 +4,7 @@ public class ServerApp {
 
     public static void main(String[] args) {
 
-        Server server = new Server();
+        Server server = new Server(8358);
         new Thread(server).start();
 
         Scanner scanner = new Scanner(System.in);
