15038bbb1d8242c3e2d98547a1e078923b203f69
rrenat358 <rrenat358@gmail.com>
2022-09-21 11:23:54 +0300

    1123

--- Lesson02-NIO/src/Server02.java
+++ Lesson02-NIO/src/Server02.java
@@ -3,6 +3,9 @@ import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Server02 implements Runnable {
 
@@ -30,6 +33,47 @@ public class Server02 implements Runnable {
 
     @Override
     public void run() {
+        System.out.println("Server started on port " + port);
+        while (true) {
+            try {
+                selector.select();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
 
+                while (iterator.hasNext()) {
+                    SelectionKey key = iterator.next();
+                    iterator.remove();
+
+                    if (!key.isValid()) {
+                        break;
+                    }
+                    if (key.isValid() && key.isAcceptable()) {
+                        acceptClient();
+                    }
+                    if (key.isValid() && key.isReadable()) {
+                        if (key.attachment() == null) {
+                            key.attach(new ChannelReader((SocketChannel) key.channel()));
+                        }
+                        ((ChannelReader) key.attachment()).read();
+                    }
+                    // todo: можно попробовать writer написать
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
+
+    private void acceptClient() {
+        try {
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            socketChannel.configureBlocking(false);
+            socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+            System.out.println("Accepted new client");
+        } catch (IOException e) {
+            System.out.println("Accept failed!");
+        }
+    }
+
+
 }
