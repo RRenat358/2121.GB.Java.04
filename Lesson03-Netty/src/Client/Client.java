bd19f4a64df483e93af7a6de8e8500cb31921c6d
rrenat358 <rrenat358@gmail.com>
2022-09-23 17:57:56 +0300

    1757

--- Lesson03-Netty/src/Client/Client.java
+++ Lesson03-Netty/src/Client/Client.java
@@ -1,9 +1,38 @@
 package Client;
 
 
+import Common.Command;
 
+import java.io.File;
+import java.nio.file.Files;
 
 public class Client {
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 13581;
+    private static final String clientDataUserPaht = "Client/DataUser/";
+
+
+    private final String host;
+    private final int port;
+
+    public Client() {
+        this(HOST, PORT);
+    }
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+
+        File file = new File(clientDataUserPaht + "my-file.txt");
+        Command command = new Command();
+
+
+
+    }
 
 
 
--- Lesson03-Netty/src/Common/Command.java
+++ Lesson03-Netty/src/Common/Command.java
@@ -1,9 +1,52 @@
 package Common;
 
 
-
+import java.io.File;
 
 public class Command {
+
+    private String command;
+    private File file;
+    private byte[] data;
+
+    public Command() {
+    }
+
+    public Command(String command, File file, byte[] data) {
+        this.command = command;
+        this.file = file;
+        this.data = data;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+
+
+
+
+
 
 
 
